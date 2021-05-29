5bafdb853775951d12353b06a27aedc1ddd5cbb0
Robert Rancz <rancz.robi@gmail.com>
2021-05-29 10:51:58 +0300

    Set up Unit of Work pattern.

---
+++ HotelListing/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using HotelListing.Data;
+using HotelListing.IRepository;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext context;
+        private IGenericRepository<Country> countries;
+        private IGenericRepository<Hotel> hotels;
+
+        public UnitOfWork(DatabaseContext databaseContext)
+        {
+            context = databaseContext;
+        }
+        public IGenericRepository<Country> Countries => countries ??= new GenericRepository<Country>(context);
+
+        public IGenericRepository<Hotel> Hotels => hotels ??= new GenericRepository<Hotel>(context);
+
+        public void Dispose()
+        {
+            context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
