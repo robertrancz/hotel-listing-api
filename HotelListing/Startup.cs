9839c37a81d1c18ea82120f9523c0a16dd927687
Robert Rancz <rancz.robi@gmail.com>
2021-05-29 13:03:24 +0300

    Add Newtonsoft Json rule for serializations with included entities to work.

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -44,7 +44,10 @@ namespace HotelListing
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel Listing", Version = "v1" });
             });
 
-            services.AddControllers();
+            services.AddControllers()
+                // The first time an object is encountered it will be serialized as usual but if the object is encountered as a child object of itself the serializer will skip serializing it.
+                // Needed for 'includes' to work properly
+                .AddNewtonsoftJson(op => op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
