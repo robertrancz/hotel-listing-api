1269ea0059e46a7dbec713d256e73c7369fb685a
Robert Rancz <rancz.robi@gmail.com>
2021-05-29 11:41:55 +0300

    Set up AutoMapper.

---
+++ HotelListing/Configurations/AutoMapperInitializer.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using HotelListing.Models;
+
+namespace HotelListing.Configurations
+{
+    public class AutoMapperInitializer : Profile
+    {
+        public AutoMapperInitializer()
+        {
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Country, CountryDtoCreate>().ReverseMap();
+            CreateMap<Hotel, HotelDto>().ReverseMap();
+            CreateMap<Hotel, HotelDtoCreate>().ReverseMap();
+        }
+    }
+}
--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,18 +1,12 @@
+using HotelListing.Configurations;
 using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HotelListing
 {
@@ -38,6 +32,8 @@ namespace HotelListing
                            .AllowAnyMethod()
                            .AllowAnyHeader());
             });
+
+            services.AddAutoMapper(typeof(AutoMapperInitializer));
 
             services.AddSwaggerGen(c =>
             {
