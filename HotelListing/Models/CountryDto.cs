a045284f2c27e2263a96fbf6c9ee52fcd2acedcd
Robert Rancz <rancz.robi@gmail.com>
2021-05-29 11:30:06 +0300

    Create data transfer objects.

---
+++ HotelListing/Models/CountryDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class CountryDtoCreate
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Country name is too long")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 2, ErrorMessage = "Short country name is too long")]
+        public string ShortName { get; set; }
+    }
+
+    public class CountryDto : CountryDtoCreate
+    {
+        public int Id { get; set; }
+        public IList<HotelDto> Hotels { get; set; }
+    }
+}
---
+++ HotelListing/Models/HotelDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class HotelDtoCreate
+    {
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "Hotel name is too long")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 250, ErrorMessage = "Hotel address is too long")]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(1,5)]
+        public double Rating { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }        
+    }
+
+    public class HotelDto : HotelDtoCreate
+    {
+        public int Id { get; set; }
+        public CountryDto Country { get; set; }
+    }
+}
